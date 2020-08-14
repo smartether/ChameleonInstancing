48d86c64c4787b2e9cf3ec27b3e637ec7fdf15b4
qzw <smartether@gmail.com>
2020-08-14 21:18:20 +0800

    1,implement instancing on solution 1.

--- Assets/ChameleonInstancingObj.cs
+++ Assets/ChameleonInstancingObj.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// solution 1: put tag meshID on mesh texcoord
+/// solution 2: distinct meshID by vertexID in vertexProgram
+/// </summary>
+
 public class ChameleonInstancingObj : MonoBehaviour
 {
+    public Renderer[] renders;
+
+    private List<Mesh> meshs = new List<Mesh>();
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var render in renders)
+        {
+            var sharedMesh = render.GetComponent<MeshFilter>().sharedMesh;
+            if (!meshs.Contains(sharedMesh))
+            {
+                meshs.Add(sharedMesh);
+            }
+        }
+
+        for(int meshIdx=0,meshMax=meshs.Count;meshIdx<meshMax;meshIdx++)
+        {
+            var mesh = meshs[meshIdx];
+            var tagUV = new Vector2[mesh.vertices.Length];
+            for(int i = 0, c = mesh.vertices.Length; i < c; i++)
+            {
+                float meshId = meshIdx / (float)meshMax;
+                tagUV[i] = new Vector2(meshId, meshId);
+            }
+            mesh.uv8 = tagUV;
+        }
     }
 
     // Update is called once per frame
